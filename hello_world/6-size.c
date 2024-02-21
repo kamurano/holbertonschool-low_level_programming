3f682a85f2400be79ccceb83f2d060f37355f3d8
kamurano <kamrannn.mk@gmail.com>
2024-02-21 17:35:06 +0000

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,8 +6,8 @@
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of a char: %d byte(s)\n", sizeof(int));
-printf("size of a char: %lu byte(s)\n", sizeof(long int));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 }
