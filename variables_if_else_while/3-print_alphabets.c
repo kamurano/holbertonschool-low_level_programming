da0b470dfc49835e5f1fc58aa53bd1121004c4c9
kamurano <kamrannn.mk@gmail.com>
2024-02-24 23:25:59 +0000

    ifelse

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Description: main func
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
