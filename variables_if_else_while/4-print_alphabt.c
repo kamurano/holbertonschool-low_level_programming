44e4a19d0d49fbfe4a4c36f1cb9fac3f065c14d5
kamurano <kamrannn.mk@gmail.com>
2024-02-25 13:21:46 +0000

    ifelse

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Description: func
+ * Return: zero
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' || a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
