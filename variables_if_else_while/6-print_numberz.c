e319f4a72489b68eba28b713976250538cd709c9
kamurano <kamrannn.mk@gmail.com>
2024-02-25 13:43:30 +0000

    ifelse

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Description: func
+ * Return: zero
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar('0' + (a % 10));
+	}
+	putchar('\n');
+	return (0);
+}
