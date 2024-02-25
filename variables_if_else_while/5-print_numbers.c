1bf4187d250512fa2d25a740e70aa679472e4c5a
kamurano <kamrannn.mk@gmail.com>
2024-02-25 13:32:22 +0000

    ifelse

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Description: func for printing
+ * Return: zero
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%i", a);
+	}
+	putchar('\n');
+	return (0);
+}
