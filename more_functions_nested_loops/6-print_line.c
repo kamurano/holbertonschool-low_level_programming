39935ecd1324d59d00c578b2f59301dd1322185f
kamurano <kamrannn.mk@gmail.com>
2024-02-26 21:56:32 +0400

    ifelse

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - func
+ * @n: var
+*/
+void print_line(int n)
+{
+	int a:
+
+	for (a = 0; a <= n; a++)
+		_putchar('_');
+	_putchar('\n';
+}
