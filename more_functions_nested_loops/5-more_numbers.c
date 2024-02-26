0eca46dc3a83ff5d4ac1a1204643a413185f37fc
kamurano <kamrannn.mk@gmail.com>
2024-02-26 21:43:34 +0400

    ifelse

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main"
+#include <stdio.h>
+/**
+ * more_numbers - func
+*/
+void more_numbers(void)
+{
+	int a, b, c;
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b <= 9)
+				_putchar('0' + b);
+			if (b > 9)
+			{
+				_putchar('1');
+				_putvhar('0' + (b % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
