f0ca14b0b6770068494d63ae64de8d86c1e47c4a
kamurano <kamrannn.mk@gmail.com>
2024-02-26 20:55:22 +0400

    ifelse

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - func
+ * @c - var
+ * Return: zero
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
