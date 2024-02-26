5ae21333ccb640426ff6f069e0691e009c90127f
kamurano <kamrannn.mk@gmail.com>
2024-02-26 17:54:35 +0400

    ifelse

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - func
+ * Description - test
+ * Return: zero
+*/
+int _isupper(int c)
+{	
+	char a;
+
+	// clar a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for (a='A'; a = 'Z'; a++);
+	{
+		if (c == a)
+		{
+			return (1)
+		}
+		else
+		{
+			return (0)
+		}
+	}
+}
