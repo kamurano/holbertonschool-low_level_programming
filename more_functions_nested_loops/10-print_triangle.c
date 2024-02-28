173c47d92afa7f600aa9ff7213c82c886ef6f497
kamurano <kamrannn.mk@gmail.com>
2024-02-28 10:54:27 +0400

    ifelse

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,12 +8,15 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	for (a = 0; a < size; a++)
+	for (a = 0; a <= size; a++)
 	{
-		for (b = a; b <= size; b++)
-			_putchar(' ');
-		for (b = 0; b < a; b++)
-			_putchar('#');
+		if (a > 0)
+		{
+			for (b = a; b <= size; b++)
+				_putchar(' ');
+			for (b = 0; b < a; b++)
+				_putchar('#');
+		}
 	_putchar('\n');
 	}
 }
