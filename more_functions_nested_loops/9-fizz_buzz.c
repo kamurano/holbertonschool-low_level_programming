b33eb582fb2c4c951c15794ba42f80ad6333705b
kamurano <kamrannn.mk@gmail.com>
2024-02-28 10:32:39 +0400

    ifelse

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <sdtio.h>
+/**
+ * main - func
+*/
+void main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+	}
+
+}
