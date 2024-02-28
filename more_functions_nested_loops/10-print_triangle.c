eeccfb71a2511b424296616f7429a978fb893a9b
kamurano <kamrannn.mk@gmail.com>
2024-02-28 10:52:10 +0400

    ifelse

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - func
+ * @size: var
+*/
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a <= size; a++)
+	{
+		for (b = a; b <= size; b++)
+			_putchar(' ');
+		for (b = 0; b < a; b++)
+			_putchar('#');
+	putchar ('\n');
+	}
+}
