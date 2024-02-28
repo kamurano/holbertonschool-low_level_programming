7d4160343f21c52a4c1121b334aea27607c48e70
kamurano <kamrannn.mk@gmail.com>
2024-02-28 21:50:18 +0400

    ifelse

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - func
+ * @s: pointer
+ * Return: zero
+*/
+int _strlen(char *s)
+{
+	return(strlen(*s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 void reset_to_98(int *n);
 int _putchar(char c);
 void print_alphabet(void);
