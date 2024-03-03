852d06e04a50eaf82420ca1a7e649fefd245e59a
kamurano <kamrannn.mk@gmail.com>
2024-03-03 21:26:35 +0400

    ifelse

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - func
+ * @str: variable
+*/
+void _puts(char *str)
+{
+	*p = str;
+
+	while (p != '\0')
+		putchar(p);
+	putchar('\n');
+}
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - func
+ * @s: variable
+*/
+void print_rev(char *s)
+{
+	char *p = s;
+
+
+}
