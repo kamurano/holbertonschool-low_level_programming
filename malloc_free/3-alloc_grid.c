13936f27d1d19bae4da2b27320a136a191b1db93
kamurano <kamrannn.mk@gmail.com>
2024-03-17 23:36:23 +0400

    strange commit

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - func
+ * @width: int1
+ * @height: int2
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
