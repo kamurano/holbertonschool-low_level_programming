f6cd69c7a36aacd99b16c806a5adc033ef374533
kamurano <kamrannn.mk@gmail.com>
2024-03-17 22:24:43 +0400

    strange commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - func
+ * @size: var
+ * @c: char
+ * Return: Null
+ */
+char *create_array(unsigned int size, char c)
+{
+
+	char *buf = malloc(sizeof(char) * (size + 1));
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	if (buf == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		buf[i] = c;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
