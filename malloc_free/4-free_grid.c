3b10080eb1704fd12927f9aeb6e198f44aa14464
kamurano <kamrannn.mk@gmail.com>
2024-03-17 23:40:24 +0400

    strange commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * free_grid - func
+ * @grid: mattr
+ * @height: int
+ * Return: pointer
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL)
+	{
+		for (i = 0; i <= height; i++)
+			free(grid[i]]);
+		free(grid);
+	}
+}
